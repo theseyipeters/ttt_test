140870c05eb73bce0b9426eecd06400de592a403
theseyipeters@gmail.com <seyi@usecrafter.com>
2025-01-10 20:45:04 +0100

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
+import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { theme } from "../../theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<MantineProvider theme={theme}>
+			<Component {...pageProps} />
+		</MantineProvider>
+	);
 }
